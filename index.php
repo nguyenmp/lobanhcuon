af17facfc5e7e3a29a344434a8514738c1d8f035
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2013-09-23 00:24:29 -0700

    Initial commit with content

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php $ROOT_PATH='http://lobanhcuon.nguyenmp.com'; ?>
+<html>
+  <head>
+    <title>Frontend of Mark Nguyen</title>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" type="text/css" href="<?php echo $ROOT_PATH; ?>/index.css" />
+  </head>
+
+  <body>
+  
+    <div id="wrapper">
+      <div id="header" >
+        <a href="<?php echo $ROOT_PATH?>" id="header-home">Lo Banh Cuon</a>
+        <div id="header-items">
+          <div class="header-item">Address: <a href="https://maps.google.com/maps?q=1281+E.+Santa+Clara+St.+San+Jose,+CA+95116">1281 E. Santa Clara St. San Jose, CA 95116</a></div>
+          <div class="header-item">Hours: Everyday 8AM-5PM or by request</div>
+          <div class="header-item">Phone: <a href="tel:1-408-995-5676">(408) 995-5676</a></div>
+        </div>
+      </div>
+
+      <div class="content">
+        <h1>
+      </div>
+
+      <div class="content">
+        <h1>Menu</h1>
+        <ul>
+          <li><h2>Rice Noodles</h2><ul>
+            <li>Plain</li>
+            <li>Onion</li>
+            <li>Shrimp</li>
+            <li>Hu Tieu</li>
+          </ul></li>
+
+          <li><h2>Meats</h2><ul>
+            <li>Pork Tubes</li>
+            <li>Deep Fried Pork Squares</li>
+            <li>Banana Leaf Pork Tubes</li>
+          </ul></li>
+
+          <li><h2>Condoments</h2><ul>
+            <li>Deep Fried Batter</li>
+            <li>Basil</li>
+            <li>Bean Sprouts</li>
+            <li>Fish Sauce Mixture (optional chili sauce)</li>
+            <li>Cucumber</li>
+          </ul></li>
+        </ul>
+      </div>
+
+    </div>
+  </body>
+</html>
